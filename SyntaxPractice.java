6c00761939c4c75b08a89c28b23b1ec35423c1de
Joseph <stevensjoseph617@gmail.com>
2025-12-03 13:29:33 +0000

    first commit

---
+++ SyntaxPractice.java
@@ -0,0 +1,20 @@
+
+
+
+class SyntaxExamples {
+    int myInt = 42;
+    double myDouble = 189.75;
+    char myChar = 'Q';
+    boolean myBool = false;
+}
+
+
+public class SyntaxPractice {
+    public static void main(String[] args) {
+
+        SyntaxExamples dt = new SyntaxExamples();
+
+        System.out.println("myInt: " + dt.myInt);
+
+    }
+}
